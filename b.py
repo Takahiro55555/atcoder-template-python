2ebb5be6af6afd83fa65adce0d9472c785703a45
Takahiro55555 <tkhr.corei7@gmail.com>
2021-05-15 20:37:28 +0900

    add: テンプレファイルを追加

---
+++ b.py
@@ -0,0 +1,17 @@
+import bisect,collections,copy,heapq,itertools,math,numpy,string
+import sys
+
+def main():
+    N, L = LI()
+    K = I()
+    A = LI()
+
+# 参考: 【Python】競プロテンプレ【AtCoder】 - Qiita
+# URL: https://qiita.com/rudorufu1981/items/c6bc5e4d72cfe1cb7f14
+def I(): return int(sys.stdin.readline().rstrip())
+def LI(): return list(map(int,sys.stdin.readline().rstrip().split()))
+def S(): return sys.stdin.readline().rstrip()
+def LS(): return list(sys.stdin.readline().rstrip().split())
+
+if __name__ == "__main__":
+    main()
