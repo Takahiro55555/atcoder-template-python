73fd684385effc55e96ba11ed8228efab3f58133
Takahiro55555 <tkhr.corei7@gmail.com>
2021-05-17 00:29:46 +0900

    update: pythonのパスに関する設定を追加

--- .setup.sh
+++ .setup.sh
@@ -11,5 +11,15 @@ fi
 
 if [ ! -f $venv_python_path ]; then
     python3 -m venv $atcoder_venv_path
-    $venv_pip3_path install numpy
+    $venv_pip3_path install numpy autopep8
+fi
+
+if [ ! -d ./.vscode ]; then
+    mkdir -p ./.vscode
+fi
+
+if [ ! -f ./.vscode/settings.json ]; then
+    echo "{" > ./.vscode/settings.json
+    echo "    \"python.pythonPath\": \"$venv_python_path\"" >> ./.vscode/settings.json
+    echo "}" >> ./.vscode/settings.json
 fi
